9c2a17a7a49be5e9b224b5431b9c548424266858
vitaly <optinsoft@gmail.com>
2024-05-29 14:33:31 +0500

    v.0.0.4 new_timestamp

--- src/unfuncaptcha_bda/fingerprint.py
+++ src/unfuncaptcha_bda/fingerprint.py
@@ -6,14 +6,21 @@ import time
 
 class ArkoseBrowserFingerprint(object):
     def __init__(self, encoded_fingerprint: str, user_agent: str, timestamp: int = int(time.time())):
+        self._user_agent = user_agent # store user_agent
         self._timestamp = str(int(timestamp - (timestamp % 21600)))
-        self._key = user_agent + str(self._timestamp)
+        self._key = self._user_agent + str(self._timestamp)
 
         self.crypto = BDACrypto(self._key)
         self._raw_fingeprint = json.loads(base64.b64decode(encoded_fingerprint)) # {"ct": ..., "s": ..., "iv": ... }
         self.fingerprint: list[dict[str, Any]] = json.loads(self.crypto.decrypt(self._raw_fingeprint).decode())
 
 
+    def new_timestamp(self, timestamp: int = int(time.time())):
+        self._timestamp = str(int(timestamp - (timestamp % 21600)))
+        self._key = self._user_agent + str(self._timestamp)
+        self.crypto = BDACrypto(self._key)
+
+    
     def repackage(self, encode_base64: bool = True) -> str:
         repacked_bda = json.dumps(
             self.crypto.re_encrypt(json.dumps(self.fingerprint, separators=(',', ':')), self._raw_fingeprint),
